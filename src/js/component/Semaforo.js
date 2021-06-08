0b1c8c0a46044b4ae1a2876d2655b05ecfa3d924
unknown <di.trgonzalez@gmail.com>
2021-06-08 17:07:12 -0400

    primer commit

---
+++ src/js/component/Semaforo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function Home() {
+	<div className="Semaforo">
+		<div className="LuzRoja"></div>
+		<div className="LuzAmarilla"></div>
+		<div className="LuzVerde"></div>
+	</div>
+}
