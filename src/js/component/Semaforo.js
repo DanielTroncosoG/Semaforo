34f7d07aa3782f1aba84f141379402e0f68974f6
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-06-09 14:55:02 +0000

    commit semaforo daniel

--- src/js/component/Semaforo.js
+++ src/js/component/Semaforo.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Semaforo() {
+	const [Color, setColor] = useState("red");
 	return (
 		<>
 			<div className="Soporte"></div>
 			<div className="GranSemaforo">
-				<div className="LuzRoja"></div>
-				<div className="LuzAmarilla"></div>
-				<div className="LuzVerde"></div>
+				<div className="Espaciado"></div>
+				<div
+					onClick={() => setColor("red")}
+					className={
+						"Luz Rojo" + (Color === "red" ? " brillo" : "")
+					}></div>
+				<div
+					onClick={() => setColor("yellow")}
+					className={
+						"Luz Amarillo" + (Color === "yellow" ? " brillo" : "")
+					}></div>
+				<div
+					onClick={() => setColor("green")}
+					className={
+						"Luz Verde" + (Color === "green" ? " brillo" : "")
+					}></div>
 			</div>
 			;
 		</>
